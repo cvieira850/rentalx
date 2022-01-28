ae2dae356f941fe3f30ebef7ef695273823dc7d5
Caio Vieira <contato@caiovieira.com.br>
2022-01-28 20:54:38 -0300

    feat: create route to list categorie

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,20 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+import { CategoriesRepositories } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoriesRepository = new CategoriesRepositories();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
-    name,
-    description,
-  };
-
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
 
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
 export { categoriesRoutes };
