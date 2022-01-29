e81e8b4a4f4a9526507f058acc88529369222394
Caio Vieira <contato@caiovieira.com.br>
2022-01-28 21:02:13 -0300

    feat: create validation on category name

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -27,5 +27,10 @@ class CategoriesRepositories {
   list(): Category[] {
     return this.categories;
   }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
 }
 export { CategoriesRepositories };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,12 @@ const categoriesRepository = new CategoriesRepositories();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already exists" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
