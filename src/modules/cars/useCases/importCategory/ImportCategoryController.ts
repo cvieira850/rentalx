d28f7f10fe6d902c205d86f136659d204b6aa188
Caio Vieira <contato@caiovieira.com.br>
2022-02-02 22:42:59 -0300

    feat: create new route to import by csv categories

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+  constructor(private readonly importCategoryUseCase: ImportCategoryUseCase) {}
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+    this.importCategoryUseCase.execute(file);
+    return response.status(201).send();
+  }
+}
+
+export { ImportCategoryController };
